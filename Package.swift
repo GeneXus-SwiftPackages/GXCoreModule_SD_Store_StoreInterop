dba58cf3b5a70f1a6c0101f51207b17c3c5c7ccd
Build Er <builder@macmini6.local>
2023-05-05 09:44:40 -0300

    Package version: 1.0.0-beta+20230505123704

--- Package.swift
+++ Package.swift
@@ -16,13 +16,13 @@ let package = Package(
 		.target(name: "GXCoreModule_SD_Store_StoreInteropWrapper",
 				dependencies: [
 					"GXCoreModule_SD_Store_StoreInterop",
-					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.watchOS, .tvOS, .iOS]))
+					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.tvOS, .iOS, .watchOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_SD_Store_StoreInterop",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Store_StoreInterop-1.0.0-beta+20230504210908.xcframework.zip",
-			checksum: "3bf3617e51c035d860cd0c946b9ab48de49a1bda95174a0bad7fb877be098fdf"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Store_StoreInterop-1.0.0-beta+20230505123704.xcframework.zip",
+			checksum: "50d40136cbee890771f907eca184d178186ca430403c35afe9ff6a96552e8f47"
 		)
 	]
 )
