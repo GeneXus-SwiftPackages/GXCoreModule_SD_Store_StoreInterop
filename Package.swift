0e5063d04a18e87acc4e541bf383169923cb4faf
Build Er <builder@macmini6.local>
2024-05-21 16:41:40 -0300

    Package version: 2.0.0-beta.34

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXCoreModule_SD_Store_StoreInteropWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "2.0.0-beta.33")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "2.0.0-beta.34")
 	],
 	targets: [
 		.target(name: "GXCoreModule_SD_Store_StoreInteropWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_SD_Store_StoreInterop",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Store_StoreInterop-2.0.0-beta.33.xcframework.zip",
-			checksum: "1c9a6294d1c45faacbc802476d9ccd1a961f75cd7b663e9d826cff2a6c05ed9c"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Store_StoreInterop-2.0.0-beta.34.xcframework.zip",
+			checksum: "995526a19ebdf9f941c98ffc0c2fbd8e89743b86ef69fd797ff0f31f6de9ae01"
 		)
 	]
 )
